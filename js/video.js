b817c0fbddad302a0d62aa50e7104f55de4aae49
sharonluyabb <165534524+sharonluyabb@users.noreply.github.com>
2024-11-21 19:55:14 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,24 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+	video = document.querySelector ("#player1");
+	video.autoplay = false;
+	video.loop = false;
+		//console log: both of autoplay and loop are set to false
+		document-querySelector ("#play").addEventListener("click", function() {
+		video.play()
+});
+
+	document.querySelector ("#slower"). addEventListener("click", function() {
+	//console log: slow down video video.playbackRate *= // slow down 10%
+	// console log: new speed is ..
+	});
+
+
+
+	document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+});
+
